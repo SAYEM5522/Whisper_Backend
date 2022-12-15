b9b5feb898bb304370490f0969db3e82edc40fca
SAYEM5522 <md1040582@gmail.com>
2022-12-15 23:41:34 +0600

    Video To Audio Add

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import youtube_dl
 from flask import Flask, jsonify, request
 import whisper
 from pathlib import Path
@@ -15,30 +16,13 @@ def hello():
     return "text"
 
 
-@app.route('/transcribe', methods=['POST'])
+@app.route('/Transcribe', methods=['POST'])
 def predict():
     if request.method == 'POST':
-        # file_data = request.get_json()['file']
-        # decode_file = base64.b64decode(file_data)
-        # print(decode_file)
+
         audio_file = request.data
         with open('w.mp3', 'wb') as f:
             f.write(audio_file)
-        # with wavfile.read('Record (online-voice-recorder.com).wav') as (audio, metadata):
-        #     n = audio.shape[1]
-        #     d = audio.shape[0]
-        # with wave.open("Record (online-voice-recorder.com).wav", 'rb') as f:
-
-        #     audio = f.readframes(-1)
-        #     audio_data = np.frombuffer(audio, dtype=np.int16)
-        #     print(audio_data)
-        # sampling_rate, data = wavfile.read(
-        #     f'Record (online-voice-recorder.com).wav')
-        # with open(f'Record (online-voice-recorder.com) (1).mp3', 'rb') as file:
-        #     mp3byte = BytesIO(file.read())
-        # mp3 = base64.b64decode(mp3byte.getvalue()).decode("ISO-8859-1")
-        # payload = np.frombuffer(mp3, dtype=np.int16)
-        # print(payload)
 
         device = "cuda" if torch.cuda.is_available() else "cpu"
         model = whisper.load_model("base")
@@ -53,14 +37,14 @@ def predict():
     return jsonify({'Text': result.text, 'language': max(probs, key=probs.get)})
 
 
-@app.route('/translate', methods=['POST'])
+@app.route('/Translate', methods=['POST'])
 def translate():
     if request.method == 'POST':
         audio_file = request.data
         with open('w.mp3', 'wb') as f:
             f.write(audio_file)
         device = "cuda" if torch.cuda.is_available() else "cpu"
-        model = whisper.load_model("base")
+        model = whisper.load_model("small")
         audio = whisper.load_audio(
             "w.mp3")
         audio = whisper.pad_or_trim(audio)
@@ -74,7 +58,7 @@ def translate():
     return jsonify({'Text': transcribe["text"]})
 
 
-@app.route('/detectLanguage', methods=['POST'])
+@app.route('/Detect Language', methods=['POST'])
 def detectLanguage():
     if request.method == 'POST':
         audio_file = request.data
@@ -87,8 +71,40 @@ def detectLanguage():
         audio = whisper.pad_or_trim(audio)
         mel = whisper.log_mel_spectrogram(audio).to(device)
         _, probs = model.detect_language(mel)
+        print(max(probs, key=probs.get))
 
     return jsonify({'language': max(probs, key=probs.get)})
+
+
+@app.route('/Video', methods=['POST'])
+def run():
+    video_url = "https://www.youtube.com/watch?v=VAjEUdN35bw"
+    video_info = youtube_dl.YoutubeDL().extract_info(
+        url=video_url, download=False
+    )
+    filename = f"{video_info['title']}.mp3"
+    options = {
+        'format': 'bestaudio/best',
+        'keepvideo': False,
+        'outtmpl': filename,
+    }
+
+    with youtube_dl.YoutubeDL(options) as ydl:
+
+        ydl.download([video_info['webpage_url']])
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model = whisper.load_model("base")
+    audio = whisper.load_audio(
+        f"{video_info['title']}.mp3")
+    audio = whisper.pad_or_trim(audio)
+    mel = whisper.log_mel_spectrogram(audio).to(device)
+    _, probs = model.detect_language(mel)
+    print(f"Detected language: {max(probs, key=probs.get)}")
+    options = whisper.DecodingOptions(fp16=False)
+    result = whisper.decode(model, mel, options)
+    print(result.text)
+    return jsonify({'Text': result.text, 'language': max(probs, key=probs.get)})
 
 
 if __name__ == '__main__':
